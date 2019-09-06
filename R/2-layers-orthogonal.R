3cbb3d29223ab06547d55208f79b34d749fd9925
Thom Quinn <contacttomquinn@gmail.com>
2019-09-06 13:20:54 +1000

    Allow user to give name prefix to function

--- R/2-layers-orthogonal.R
+++ R/2-layers-orthogonal.R
@@ -7,19 +7,24 @@
 #'
 #' @param target The incoming layer with predictive power.
 #' @param reference The incoming layer you do not want to predict.
-#' @param name A string. The name of the layers.
+#' @param name A string. The prefix label for all layers.
 #' @return A layer that equals the "reference" when the "target" is
 #'  orthogonal to the "reference".
 #' @importFrom keras layer_dot layer_add
 #' @export
 layer_orthogonal_to <- function(target, reference, name = NULL){
 
-  if(is.null(name)) name <- unlist(strsplit(target$name, "/|:"))[1]
+  # Name layer based on incoming data
+  if(is.null(name)){
+    name <- get_incoming_layer_name(target)
+  }
+
   name_reference <- unlist(strsplit(reference$name, "/|:"))[1]
   name_perpendicular <- paste0(name, "_orthogonal_to_", name_reference)
   name_addback <- paste0(name, "_add_back_", name_reference)
 
   message("Alert: This layer equals ", name_reference, " when input is orthogonal to it.")
+
   zero_if_perpendicular <- layer_dot(list(target, reference), axes = 1, name = name_perpendicular)
   layer_add(list(zero_if_perpendicular, reference), name = name_addback)
 }
