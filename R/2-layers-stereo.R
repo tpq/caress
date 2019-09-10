b6f264541e79cf38b63f8f16a1898cf48de95844
Thom Quinn <contacttomquinn@gmail.com>
2019-09-10 11:33:34 +1000

    Use explicit args for layer_stereo* and add channels

--- R/2-layers-stereo.R
+++ R/2-layers-stereo.R
@@ -6,18 +6,20 @@
 #'
 #' @param object A \code{keras} model.
 #' @param name A string. The prefix label for all layers.
-#' @param ... Arguments passed to \code{keras::layer_dense}.
+#' @param units,activation,... Arguments passed to \code{keras::layer_dense}.
 #' @return A list of two layers: Channel 1 and Channel 2.
 #' @export
-layer_to_stereo <- function(object, name = NULL, ...){
+layer_to_dense_stereo <- function(object, units, activation = NULL, name = NULL, ...){
 
   # Name layer based on incoming data
   if(is.null(name)){
     name <- get_incoming_layer_name(object)
   }
 
-  channel1 <- object %>% keras::layer_dense(..., name = paste0(name, "_channel", 1))
-  channel2 <- object %>% keras::layer_dense(..., name = paste0(name, "_channel", 2))
+  channel1 <- object %>% keras::layer_dense(units = units, activation = activation,
+                                            name = paste0(name, "_channel", 1), ...)
+  channel2 <- object %>% keras::layer_dense(units = units, activation = activation,
+                                            name = paste0(name, "_channel", 2), ...)
 
   return(list("channel1" = channel1,
               "channel2" = channel2))
@@ -27,24 +29,26 @@ layer_to_stereo <- function(object, name = NULL, ...){
 #'
 #' This function splits the input into three parallel layers.
 #'  The first two layers have the same architecture, but are influenced
-#'  by different weights. The third layer is the concatenation of
+#'  by different weights. The third layer is the sum of
 #'  the other two layers.
 #'
-#' @inheritParams layer_to_stereo
+#' @inheritParams layer_to_dense_stereo
 #' @return A list of three layers: Channel 1, Channel 2, and
 #'  Channel 1 & Channel 2.
 #' @export
-layer_to_stereo_and_mono <- function(object, name = NULL, ...){
+layer_to_dense_stereo_and_mono <- function(object, units, activation = NULL, name = NULL, ...){
 
   # Name layer based on incoming data
   if(is.null(name)){
     name <- get_incoming_layer_name(object)
   }
 
-  channel1 <- object %>% keras::layer_dense(..., name = paste0(name, "_channel", 1))
-  channel2 <- object %>% keras::layer_dense(..., name = paste0(name, "_channel", 2))
-  channel12 <- keras::layer_concatenate(list(channel1, channel2),
-                                        name = paste0(name, "_channel", 12))
+  channel1 <- object %>% keras::layer_dense(units = units, activation = activation,
+                                            name = paste0(name, "_channel", 1), ...)
+  channel2 <- object %>% keras::layer_dense(units = units, activation = activation,
+                                            name = paste0(name, "_channel", 2), ...)
+  channel12 <- keras::layer_add(list(channel1, channel2),
+                                name = paste0(name, "_channel", 12))
 
   return(list("channel1" = channel1, "channel2" = channel2,
               "channel12" = channel12))
