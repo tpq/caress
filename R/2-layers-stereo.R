9e105c790cbec5e23d80fe3d67eea9330cc572e5
Thom Quinn <contacttomquinn@gmail.com>
2019-09-13 20:12:34 +1000

    Create more stereo_and_* layers (add, diff, cat)

--- R/2-layers-stereo.R
+++ R/2-layers-stereo.R
@@ -25,7 +25,36 @@ layer_to_dense_stereo <- function(object, units, activation = NULL, name = NULL,
               "channel2" = channel2))
 }
 
-#' Split Model into Three Parallel Layers
+#' Split Model into Two Parallel Layers
+#'
+#' This function splits the input into three parallel layers.
+#'  The first two layers have the same architecture, but are influenced
+#'  by different weights. The third layer is the concatenation of
+#'  the other two layers.
+#'
+#' @inheritParams layer_to_dense_stereo
+#' @return A list of three layers: Channel 1, Channel 2, and
+#'  Channel 1 & Channel 2.
+#' @export
+layer_to_dense_stereo_and_cat <- function(object, units, activation = NULL, name = NULL, ...){
+
+  # Name layer based on incoming data
+  if(is.null(name)){
+    name <- get_incoming_layer_name(object)
+  }
+
+  channel1 <- object %>% keras::layer_dense(units = units, activation = activation,
+                                            name = paste0(name, "_channel", 1), ...)
+  channel2 <- object %>% keras::layer_dense(units = units, activation = activation,
+                                            name = paste0(name, "_channel", 2), ...)
+  channel12 <- keras::layer_concatenate(list(channel1, channel2),
+                                        name = paste0(name, "_channel", 12))
+
+  return(list("channel1" = channel1, "channel2" = channel2,
+              "channel12" = channel12))
+}
+
+#' Split Model into Two Parallel Layers
 #'
 #' This function splits the input into three parallel layers.
 #'  The first two layers have the same architecture, but are influenced
@@ -36,7 +65,7 @@ layer_to_dense_stereo <- function(object, units, activation = NULL, name = NULL,
 #' @return A list of three layers: Channel 1, Channel 2, and
 #'  Channel 1 & Channel 2.
 #' @export
-layer_to_dense_stereo_and_mono <- function(object, units, activation = NULL, name = NULL, ...){
+layer_to_dense_stereo_and_add <- function(object, units, activation = NULL, name = NULL, ...){
 
   # Name layer based on incoming data
   if(is.null(name)){
@@ -49,6 +78,35 @@ layer_to_dense_stereo_and_mono <- function(object, units, activation = NULL, nam
                                             name = paste0(name, "_channel", 2), ...)
   channel12 <- keras::layer_add(list(channel1, channel2),
                                 name = paste0(name, "_channel", 12))
+
+  return(list("channel1" = channel1, "channel2" = channel2,
+              "channel12" = channel12))
+}
+
+#' Split Model into Two Parallel Layers
+#'
+#' This function splits the input into three parallel layers.
+#'  The first two layers have the same architecture, but are influenced
+#'  by different weights. The third layer is the difference of
+#'  the other two layers.
+#'
+#' @inheritParams layer_to_dense_stereo
+#' @return A list of three layers: Channel 1, Channel 2, and
+#'  Channel 1 & Channel 2.
+#' @export
+layer_to_dense_stereo_and_diff <- function(object, units, activation = NULL, name = NULL, ...){
+
+  # Name layer based on incoming data
+  if(is.null(name)){
+    name <- get_incoming_layer_name(object)
+  }
+
+  channel1 <- object %>% keras::layer_dense(units = units, activation = activation,
+                                            name = paste0(name, "_channel", 1), ...)
+  channel2 <- object %>% keras::layer_dense(units = units, activation = activation,
+                                            name = paste0(name, "_channel", 2), ...)
+  channel12 <- keras::layer_subtract(list(channel1, channel2),
+                                     name = paste0(name, "_channel", 12))
 
   return(list("channel1" = channel1, "channel2" = channel2,
               "channel12" = channel12))
